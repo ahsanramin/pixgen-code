e0cfa45be44e2d4d1defc89b9ef55d10536ee79e
ahsanramin <ahsanramin25@gmail.com>
2026-04-30 22:08:39 +0600

    my 3rd commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
 import Banner from "@/components/Banner";
-import TopGenerations from "@/components/TopGenerations";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
       <Banner/>
-
-      <TopGenerations/>
-   
     </div>
   );
 }
